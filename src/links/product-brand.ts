93d00567493cdd94eb128329f7548383ef524bb8
Ace <ace.jl.han@gmail.com>
2025-03-07 15:40:23 +0800

    Query Linked Records

--- src/links/product-brand.ts
+++ src/links/product-brand.ts
@@ -1,11 +1,13 @@
-import BrandModule from "../modules/brand"
-import ProductModule from "@medusajs/medusa/product"
-import { defineLink } from "@medusajs/framework/utils"
+import BrandModule from "../modules/brand";
+import ProductModule from "@medusajs/medusa/product";
+import { defineLink } from "@medusajs/framework/utils";
 
+// after npx medusa db:migrate or npx medusa db:sync-links
+// will create a table named like ${module1}_${module1_model1}_${module2}_${module2_model1}
 export default defineLink(
   {
     linkable: ProductModule.linkable.product,
     isList: true,
   },
   BrandModule.linkable.brand
-)
+);
